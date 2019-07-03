062bf22184b5ceffe24e922bb91825a975ccce46
gcx <502134295@qq.com>
2019-07-03 21:22:52 +0800

    trust you no longer

--- gcxreader2.py
+++ gcxreader2.py
@@ -1,12 +1,14 @@
 #!/bin/python
+lastReadingCharacters = 10000
 width = 1100
 height = 800
-columnMargin = 5
-rowMargin = 5
+columnMargin = 100
+rowMargin = 10
 centerMargin = 80
 fontSize = 14
 lastRow = 10
-lastReadingCharacters = 10000
+foreGroundColor = 'grey'
+backGroundColor = 'white'
 #### Imports
 import _thread
 from tkinter import *
@@ -14,15 +16,13 @@ import tkinter.font as tkFont
 import codecs
 import time
 
-#### New thread to prepare to write sitting (codes)
-def readMyself():
-    pythonFile = open(__file__, 'r')
-    codes = pythonFile.read()
-_thread.start_new_thread(readMyself, ())
 #### GUI init
 rootWindow = Tk()
 rootWindow.title('gcxReader')
-rootWindow.geometry(str(width) + 'x' + str(height))
+windowPositionX = (rootWindow.winfo_screenwidth() - width) / 2
+windowPositionY = (rootWindow.winfo_screenheight() - height) / 2
+rootWindow.geometry('%dx%d+%d+%d' % (width, height, windowPositionX, windowPositionY))
+rootWindow.configure(bg = backGroundColor)
 
 #### Read bookValue
 normalFont = tkFont.Font(family = 'Source Code Pro', size = fontSize)
@@ -38,6 +38,17 @@ textLength = len(text)
 calculatePointer = lastReadingCharacters
 maxColumn = []
 maxLine = 0
+pythonFile = 0
+otherCodes = 0
+#### New thread to prepare to write sitting (codes)
+def readMyself():
+    global pythonFile
+    global otherCodes
+    pythonFile = open(__file__, 'r')
+    pythonFile.readline()
+    pythonFile.readline()
+    otherCodes = pythonFile.read()
+_thread.start_new_thread(readMyself, ())
 
 #### thread calculating
 def calculateMaxColumn():
@@ -45,11 +56,10 @@ def calculateMaxColumn():
     global calculatePointer
     global maxLine
     global text
-    tempCharPixelWidth = normalFont.measure('cccc')
     tempLimit = (width - columnMargin * 2 - centerMargin) // 2
     while calculatePointer < textLength:
         tempLine = ''
-        pixelFontLength = 0#tempCharPixelWidth
+        pixelFontLength = fontSize * 1.5
         while pixelFontLength < tempLimit and calculatePointer < textLength:
             if text[calculatePointer] == '\n' or text[calculatePointer] == '\r':
                 calculatePointer += 1
@@ -74,21 +84,27 @@ def turnPage(event):
     for widget in rootWindow.winfo_children():
         widget.destroy()
     expectedRow = min(nowRow + rowLimit, maxLine)
-    originRow = nowRow
-    xStart = rowMargin / height
-    yStart = columnMargin / width
     rowHeight = (height - 2 * rowMargin) / rowLimit / height
-    singlePageWidth = (width - columnMargin - centerMargin - columnMargin) / 2 / width
+    originRow = nowRow
+    relx = columnMargin / height
+    yStart = columnMargin / width
+    relheight = (height - 2 * rowMargin) / rowLimit / height
+    relwidth = (width - columnMargin - centerMargin - columnMargin) / 2 / width
     while nowRow < expectedRow:
-        Label(rootWindow, text = text2[maxColumn[nowRow]:maxColumn[nowRow + 1]], font = normalFont, justify = LEFT, anchor = N + W).place(relheight = rowHeight, relwidth = singlePageWidth, relx = xStart, rely = (nowRow - originRow) *  rowHeight + columnMargin / width)
+        rely = (nowRow - originRow) *  rowHeight + rowMargin / width
+        Label(rootWindow, text = text2[maxColumn[nowRow]:maxColumn[nowRow + 1]], font = normalFont, fg = foreGroundColor, bg = backGroundColor, justify = LEFT, anchor = N + W).place(relheight = relheight, relwidth = relwidth, relx = relx, rely = rely)
         nowRow += 1
 
-    xStart = (width / 2 + centerMargin / 2) / width
+
     expectedRow = min(nowRow + rowLimit, maxLine)
     originRow = nowRow
+    relx = (width / 2 + centerMargin / 2) / width
     while nowRow < expectedRow:
-        Label(rootWindow, text = text2[maxColumn[nowRow]:maxColumn[nowRow + 1]], font = normalFont, justify = LEFT, anchor = N + W).place(relheight = rowHeight, relwidth = singlePageWidth, relx = xStart, rely = (nowRow - originRow) * rowHeight + columnMargin / width)
+        rely = (nowRow - originRow) *  rowHeight + rowMargin / width
+        Label(rootWindow, text = text2[maxColumn[nowRow]:maxColumn[nowRow + 1]], font = normalFont, fg = foreGroundColor, bg = backGroundColor, justify = LEFT, anchor = N + W).place(relheight = relheight, relwidth = relwidth, relx = relx, rely = rely)
         nowRow += 1
+
+        rootWindow.title('%.1f' % (maxColumn[nowRow - 1] / textLength * 100) + '%')
 
 #### Events listener
 def turnLeft(event):
@@ -100,10 +116,17 @@ def turnLeft(event):
     nowRow = rowStack.pop()
     turnPage(event)
 
+#### Close with Saving
+def closeWithSaving():
+    global otherCodes
+    newScriptFileValue = '#!/bin/python\nlastReadingCharacters = ' + str(maxColumn[nowRow]) + '\n' + otherCodes
+    print(newScriptFileValue)
+    exit(0)
+
 #### Environment init
 welcomeMessage = "I am preparing for reading, please wait a minute..."
 welcomeMessagePixelWidth = normalFont.measure(welcomeMessage)
-Label(rootWindow, text = welcomeMessage, font = normalFont, justify = LEFT, anchor = N + W).place(relheight = 1, relwidth = 1, relx = (width - welcomeMessagePixelWidth) / 2 / width, rely = (height - fontSize) / 2 / height)
+Label(rootWindow, text = welcomeMessage, font = normalFont, justify = LEFT, fg = foreGroundColor, bg = backGroundColor, anchor = N + W).place(relheight = 1, relwidth = 1, relx = (width - welcomeMessagePixelWidth) / 2 / width, rely = (height - fontSize) / 2 / height)
 def closeWelcomeMessage():
     time.sleep(1)
     rootWindow.winfo_children()[0].destroy()
@@ -113,5 +136,5 @@ _thread.start_new_thread(closeWelcomeMessage, ())
 #### Events binds
 rootWindow.bind('<Right>', turnPage)
 rootWindow.bind('<Left>', turnLeft)
-
+rootWindow.protocol('WM_DELETE_WINDOW', closeWithSaving)
 mainloop()
